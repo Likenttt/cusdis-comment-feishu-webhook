ff8c5ef1ce5707def050d9a99c1fb4a8b31beb12
lichuanyi <chuanyi.work@gmail.com>
2022-10-22 16:20:44 +0800

    update response

--- pages/api/[webhook_token].ts
+++ pages/api/[webhook_token].ts
@@ -4,13 +4,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const chatBot = require('vanilla-feishu-robot');
 
 type Data = {
-    name: string
+    message: string
 }
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
+
     /**
      * {
      *   "type": "new_comment",
@@ -28,48 +29,56 @@ export default function handler(
         // req.query.webhook_token -> "c6f7764e5284feb3050c8fa6bp84b090a"
         // pages/api/[webhook_token].ts -> /api/c6f7764e5284feb3050c8fa6bp84b090a
         // req.query.webhook_token -> "c6f7764e5284feb3050c8fa6bp84b090a"
-    const { webhook_token } = req.query
-    if(!webhook_token || webhook_token != process.env.WEBHOOK_TOKEN){
-        res.status(401)
+    const {webhook_token} = req.query
+    console.log('webhook_token is: ' + webhook_token)
+    if (!webhook_token || webhook_token != process.env.WEBHOOK_TOKEN) {
+        res.status(401).json({
+            message: "Forbidden"
+        })
     }
     const {type, data} = req.body
-    if (!type || type !== 'new_comment') {
-        res.status(200)
-        return
-    }
-    const {by_nickname, by_email, content, page_id, page_title, project_title, approve_link} = data
-    const feishuBot = new chatBot({
-        webhook: process.env.FEISHU_WEBHOOK_URL,
-        secret: process.env.FEISHU_SECRET,
-    })
+    console.log('req.body is: ' + req.body)
 
-    const message = {
-        "zh_cn": {
-            "title": `New comment on "${project_title}"`,
-            "content": [
-                [
-                    {
-                        "tag": "text",
-                        "text": `${by_nickname}|${by_email} comments in page "${page_title}" `
-                    },
-                    {
-                        "tag": "text",
-                        "text": content
-                    },
+    if (type && type == 'new_comment') {
+        const {by_nickname, by_email, content, page_id, page_title, project_title, approve_link} = data
+        const feishuBot = new chatBot({
+            webhook: process.env.FEISHU_WEBHOOK_URL,
+            secret: process.env.FEISHU_SECRET,
+        })
 
-                    {
-                        "tag": "a",
-                        "text": "Approve/Reply Here",
-                        "href": approve_link
-                    },
-                    // {
-                    //   "tag": "at",
-                    //   "user_id": "ou_18eac8********17ad4f02e8bbbb"
-                    // }
+        const message = {
+            "zh_cn": {
+                "title": `New comment on "${project_title}"`,
+                "content": [
+                    [
+                        {
+                            "tag": "text",
+                            "text": `${by_nickname}|${by_email} comments in page "${page_title}" `
+                        },
+                        {
+                            "tag": "text",
+                            "text": content
+                        },
+
+                        {
+                            "tag": "a",
+                            "text": "Approve/Reply Here",
+                            "href": approve_link
+                        },
+                        // {
+                        //   "tag": "at",
+                        //   "user_id": "ou_18eac8********17ad4f02e8bbbb"
+                        // }
+                    ]
                 ]
-            ]
+            }
         }
+        feishuBot.richText(message)
+        res.status(200).json({
+            message: "OJBK"
+        })
     }
-    feishuBot.richText(message)
-    res.status(200)
+    res.status(200).json({
+        message: "NOTHING PERFORMED"
+    })
 }
